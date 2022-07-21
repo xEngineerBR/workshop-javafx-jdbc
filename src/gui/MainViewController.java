f4a5519e522b0a6860ff6ed1fd21fcb9beba302c
Lucas <lucas.souzasantos@hotmail.com.br>
2022-07-21 19:16:45 -0300

    DepartmentList view design

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,3 +1,4 @@
+
 package gui;
 
 import java.io.IOException;
@@ -31,12 +32,12 @@ public class MainViewController implements Initializable {
 	public void onMenuItemSellerAction() {
 		System.out.println("onMenuItemSellerAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		System.out.println("onMenuItemDepartmentAction");
+		loadView("/gui/DepartmentList.fxml");
 	}
-	
+
 	@FXML
 	public void onMenuItemAboutAction() {
 		loadView("/gui/About.fxml");
@@ -45,22 +46,21 @@ public class MainViewController implements Initializable {
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 	}
-	
-	private void loadView(String absoluteName) {
+
+	private synchronized void loadView(String absoluteName) {
 		try {
-		    FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-		    VBox newVBox = loader.load();
-		 
-		    Scene mainScene = Main.getMainScene();
-		    VBox mainVBox = (VBox) ((ScrollPane)mainScene.getRoot()).getContent();
-		    
-		    Node mainMenu = mainVBox.getChildren().get(0);
-		    mainVBox.getChildren().clear();
-		    mainVBox.getChildren().add(mainMenu);
-		    mainVBox.getChildren().addAll(newVBox.getChildren());
-		}
-		catch (IOException e) {
-			Alerts.showAlert("IO Exception",  "Error loading view", e.getMessage(), AlertType.ERROR);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
 }
