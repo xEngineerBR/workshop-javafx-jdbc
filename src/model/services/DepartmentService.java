1458e01c7e3d5cf1339cdfcbe38aaa0e87fba2df
Lucas <lucas.souzasantos@hotmail.com.br>
2022-07-22 20:01:48 -0300

    Saving a new Department

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,6 +1,5 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -13,5 +12,14 @@ public class DepartmentService {
 	
 	public List<Department> findAll(){
 		return dao.findAll();
+	}
+	
+	public void saveOrUpdate(Department obj) {
+		if (obj.getId() == null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
 	}
 }
